c048aac8276efc6d83d6baef7bd00506db302bde
Le Minh Tan <v.tanlm2@vinfast.vn>
2021-07-29 21:52:15 +0700

    upload download file using multipart/form-data

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/dutroctu/go-webservice/database"
 	"github.com/dutroctu/go-webservice/product"
+	"github.com/dutroctu/go-webservice/receipt"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -24,6 +25,7 @@ const apiBasePath = "/api"
 
 func main() {
 	database.SetupDatabase()
+	receipt.SetupRoutes(apiBasePath)
 	product.SetupRoutes(apiBasePath)
 
 	http.ListenAndServe(":5000", nil)
